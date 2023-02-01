e5b877c0dca40f65277b773cb8decc916bdd3fdb
Jarmel <pawel.jarmula@gmail.com>
2023-02-01 20:11:51 +0100

    first stylesadded, html improved

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const playerArea = document.querySelector('.player')
-const enemyArea = document.querySelector('.enemy')
+const playerArea = document.querySelector('.char-player')
+const enemyArea = document.querySelector('.char-enemy')
 const attackBtn = document.querySelector('.attack-btn')
 const specialAttackBtn = document.querySelector('.special-attack-btn')
 const focusBtn = document.querySelector('.focus-btn')
-const playerName = document.querySelector('.player .name')
-const playerAttack = document.querySelector('.player .attack')
-const playerDefence = document.querySelector('.player .defence')
-const playerHealth = document.querySelector('.player .health')
-const enemyName = document.querySelector('.enemy .name')
-const enemyAttack = document.querySelector('.enemy .attack')
-const enemyDefence = document.querySelector('.enemy .defence')
-const enemyHealth = document.querySelector('.enemy .health')
+const playerName = document.querySelector('.stats-player .name')
+const playerAttack = document.querySelector('.stats-player .attack')
+const playerDefence = document.querySelector('.stats-player .defence')
+const playerHealth = document.querySelector('.stats-player .health')
+const enemyName = document.querySelector('.stats-enemy .name')
+const enemyAttack = document.querySelector('.stats-enemy .attack')
+const enemyDefence = document.querySelector('.stats-enemy .defence')
+const enemyHealth = document.querySelector('.stats-enemy .health')
+const imgMonster1 = document.querySelector('.img-monster1')
 let power;
 let damage;
 const activities = [launchAttack,launchSpecialAttack,launchFocus]
@@ -19,7 +20,7 @@ class Character {
     constructor(name,strength,defence,image){
         this.name=name,
         this.strength=strength,
-        this.defence=defence
+        this.defence=defence,
         this.image=image
     }
 
@@ -46,12 +47,16 @@ class Monster extends Character {
     specialAttack() {
         power=this.strength*Math.ceil(Math.random()*3)
     }
+
+    showImage() {
+        this.image.style.display='block'
+    }
 }
-const monster1 = new Monster('Monster1',20,10,'www')
-const monster2 = new Monster('Monster2',15,15,'www')
-const monster3 = new Monster('Monster3',10,20,'www')
-const monsters = [monster1,monster2,monster3]
-const enemy = monsters[Math.round(Math.random()*2)]
+const monster1 = new Monster('Monster1',20,10,imgMonster1)
+const monster2 = new Monster('Monster2',15,15,imgMonster1)
+const monsters = [monster1,monster2]
+const enemy = monsters[Math.round(Math.random())]
+enemy.showImage()
 
 let turn = player
 
